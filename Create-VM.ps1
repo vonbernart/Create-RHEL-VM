dac0cf992b089745d489a3afce5bf7b871865160
Theodoor Bernart <theodoor@bernart.nl>
2023-03-21 11:16:56 +0100

    update create vm 4

--- Create-VM.ps1
+++ Create-VM.ps1
@@ -7,12 +7,15 @@ $VM="Test-VM-Hyper-V"
 $MSB="1GB"
 $NVP="\\DC01\data-e\vms\Test-VN\Test-M.vhdx"
 $NVSB="30GB"
-New-VM -Name "Test-VM-Hyper-V" -MemoryStartupBytes 1GB -NewVHDPath d:\vms\Test-M.vhdx -NewVHDSizeBytes 30GB -Dynamic
--BootDevice CD -SwitchName 
+$Switch="Extern"
+$PathISO="\\DC01\Data-E\ISO\AlmaLinux-8.iso"
+$VMServer="hv-lt-01"
+New-VM -Name $VM -MemoryStartupBytes $MSB -NewVHDPath $NVP -NewVHDSizeBytes $NVSB -Dynamic 
+-SwitchName $Switch -Credential $Credential -ComputerName $VMServer
 
-Set-VMDvdDrive -VMName DC -ControllerNumber 1 -Path "<path to ISO>
+Set-VMDvdDrive -VMName DC -ControllerNumber 3 -Path $PathISO
 
-# Start-VM -Name "Test-VM-Hyper-V"
+Start-VM -Name "Test-VM-Hyper-V"
 
 # Get-VM -Name "Test-VM-Hyper-V"
 
