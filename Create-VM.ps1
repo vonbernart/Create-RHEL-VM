e8acf3c2191c49ac713462e922eb9e04c19244c8
Theodoor Bernart <theodoor@bernart.nl>
2023-03-21 09:49:08 +0100

    update create vm 3

--- Create-VM.ps1
+++ Create-VM.ps1
@@ -2,19 +2,25 @@ update-help -Force
 
 Get-Help New-VM -Examples
 
+$Credential=Get-Credential
 $VM="Test-VM-Hyper-V"
+$MSB="1GB"
+$NVP="\\DC01\data-e\vms\Test-VN\Test-M.vhdx"
+$NVSB="30GB"
+New-VM -Name "Test-VM-Hyper-V" -MemoryStartupBytes 1GB -NewVHDPath d:\vms\Test-M.vhdx -NewVHDSizeBytes 30GB -Dynamic
+-BootDevice CD -SwitchName 
 
-New-VM -Name "Test-VM-Hyper-V" -MemoryStartupBytes 1GB -NewVHDPath d:\vms\Test-M.vhdx -NewVHDSizeBytes 30GB
+Set-VMDvdDrive -VMName DC -ControllerNumber 1 -Path "<path to ISO>
 
-Start-VM -Name "Test-VM-Hyper-V"
+# Start-VM -Name "Test-VM-Hyper-V"
 
-Get-VM -Name "Test-VM-Hyper-V"
+# Get-VM -Name "Test-VM-Hyper-V"
 
-Stop-VM -Name "Test-VM-Hyper-V" -Force
+# Stop-VM -Name "Test-VM-Hyper-V" -Force
 
-Remove-VM -Name "Test-VM-Hyper-V" -Force
+# Remove-VM -Name "Test-VM-Hyper-V" -Force
 
-Get-VM
+# Get-VM
 
-Remove-Item -Path D:\VMS\Test-M.vhdx -Force
+# Remove-Item -Path D:\VMS\Test-M.vhdx -Force
 
